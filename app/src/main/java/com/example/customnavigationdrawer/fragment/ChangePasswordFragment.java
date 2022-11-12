4855e8edc05a3976f9302fcaf4db735dfe6bd021
_april.inme <dung8ahxh@gmail.com>
2022-11-12 15:32:18 +0700

    Authenticate

--- app/src/main/java/com/example/customnavigationdrawer/fragment/ChangePasswordFragment.java
+++ app/src/main/java/com/example/customnavigationdrawer/fragment/ChangePasswordFragment.java
@@ -17,6 +17,8 @@ import androidx.fragment.app.Fragment;
 import com.example.customnavigationdrawer.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -68,11 +70,37 @@ public class ChangePasswordFragment extends Fragment {
                             progressDialog.cancel();
                         }
                         else{
+
                             try {
                                 throw task.getException();
                             } catch(Exception e) {
                                 Log.e(TAG, e.getMessage());
                             }
+                        }
+                    }
+                });
+    }
+
+    private void reAuthenticate(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get auth credentials from the user for re-authentication. The example below shows
+        // email and password credentials but there are multiple possible providers,
+        // such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider
+                .getCredential("user@example.com", "password1234");
+
+        // Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "User re-authenticated.");
+                        if(task.isSuccessful()){
+                            onClickChangePassword();
+                        }
+                        else {
+
                         }
                     }
                 });
